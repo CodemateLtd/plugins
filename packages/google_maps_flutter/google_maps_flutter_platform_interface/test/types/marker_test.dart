cc632983d2ccd255c5cc0381ba95e4552e6a3115
Joonas Kerttula <joonas.kerttula@codemate.com>
2022-11-23 18:50:06 +0200

    [google_maps_flutter] remove unused import

--- packages/google_maps_flutter/google_maps_flutter_platform_interface/test/types/marker_test.dart
+++ packages/google_maps_flutter/google_maps_flutter_platform_interface/test/types/marker_test.dart
@@ -2,8 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'dart:html';
-
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:google_maps_flutter_platform_interface/google_maps_flutter_platform_interface.dart';
