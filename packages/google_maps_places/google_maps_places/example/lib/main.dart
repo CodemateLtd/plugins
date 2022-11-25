e2640159317b626f34a3f0f382520cebee03d1e6
Timo Pieti <timo.pieti@codemate.com>
2022-11-25 13:55:22 +0200

    Refactor example

--- packages/google_maps_places/google_maps_places/example/lib/main.dart
+++ packages/google_maps_places/google_maps_places/example/lib/main.dart
@@ -2,242 +2,44 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'package:collection/collection.dart';
+// ignore_for_file: public_member_api_docs
 
 import 'package:flutter/material.dart';
-import 'package:google_maps_places/google_maps_places.dart';
 
-/// Title
-const String title = 'Flutter Google Maps Places Example';
+import 'auto_complete.dart';
+import 'page.dart';
 
-void main() {
-  runApp(MyApp());
-}
+final List<PlacesExampleAppPage> _allPages = <PlacesExampleAppPage>[
+  const AutoCompletePage(),
+];
 
-/// Main app
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: title,
-      theme: ThemeData(
-        primaryColor: Colors.blueAccent,
-      ),
-      home: MyHomePage(),
-    );
+class PlacesDemo extends StatelessWidget {
+  const PlacesDemo({super.key});
+
+  void _pushPage(BuildContext context, PlacesExampleAppPage page) {
+    Navigator.of(context).push(MaterialPageRoute<void>(
+        builder: (_) => Scaffold(
+              appBar: AppBar(title: Text(page.title)),
+              body: page,
+            )));
   }
-}
-
-/// Main home page
-class MyHomePage extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  //
-  String? _predictLastText;
-  List<String> _countries = ['fi'];
-  TypeFilter _placeTypeFilter = TypeFilter.address;
-
-  final LatLngBounds _locationBias = LatLngBounds(
-    southwest: const LatLng(60.4518, 22.2666),
-    northeast: const LatLng(70.0821, 27.8718),
-  );
-
-  bool _predicting = false;
-  dynamic _predictErr;
-
-  List<AutocompletePrediction?>? _predictions;
-
-  final TextEditingController _fetchPlaceIdController = TextEditingController();
-
-  bool _fetching = false;
-  dynamic _fetchingErr;
-
-  late Future<void> _loading;
-
-  @override
-  void initState() {
-    super.initState();
-    _loading = init();
-  }
-
-  Future<bool> init() => Future.delayed(
-        const Duration(seconds: 1),
-        () => true,
-      );
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: const Text(title)),
-        body: FutureBuilder(
-          future: _loading,
-          builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
-            if (snapshot.connectionState == ConnectionState.done) {
-              if (snapshot.hasError) {
-                return Center(child: Text(snapshot.error?.toString() ?? "N/A"));
-              }
-
-              return _buildBody();
-            }
-
-            return Center(child: CircularProgressIndicator());
-          },
-        ));
-  }
-
-  Widget _buildBody() {
-    final List<Widget> predictionsWidgets = _buildPredictionWidgets();
-
-    return Padding(
-      padding: EdgeInsets.all(30),
-      child: ListView(children: predictionsWidgets),
+      appBar: AppBar(title: const Text('Google Maps Places examples')),
+      body: ListView.builder(
+        itemCount: _allPages.length,
+        itemBuilder: (_, int index) => ListTile(
+          leading: _allPages[index].leading,
+          title: Text(_allPages[index].title),
+          onTap: () => _pushPage(context, _allPages[index]),
+        ),
+      ),
     );
   }
+}
 
-  void _onPlaceTypeFilterChanged(TypeFilter? value) {
-    if (value != null) {
-      setState(() {
-        _placeTypeFilter = value;
-      });
-    }
-  }
-
-  String? _countriesValidator(String? input) {
-    if (input == null || input.length == 0) {
-      return null; // valid
-    }
-
-    return input
-        .split(",")
-        .map((String part) => part.trim())
-        .map((String part) {
-          if (part.length != 2) {
-            return "Country part '${part}' must be 2 characters";
-          }
-          return null;
-        })
-        .where((String? item) => item != null)
-        .firstOrNull;
-  }
-
-  void _onCountriesTextChanged(String countries) {
-    _countries = (countries == "")
-        ? []
-        : countries
-            .split(",")
-            .map((String item) => item.trim())
-            .toList(growable: false);
-  }
-
-  void _onPredictTextChanged(String value) {
-    _predictLastText = value;
-  }
-
-  void _predict() async {
-    if (_predicting) {
-      return;
-    }
-
-    final bool hasContent = _predictLastText?.isNotEmpty ?? false;
-
-    setState(() {
-      _predicting = hasContent;
-      _predictErr = null;
-    });
-
-    if (!hasContent) {
-      return;
-    }
-    try {
-      final List<AutocompletePrediction> result =
-          await GoogleMapsPlaces.findAutocompletePredictions(
-              query: _predictLastText!,
-              countries: _countries,
-              typeFilter: <TypeFilter>[_placeTypeFilter],
-              origin: const LatLng(65.0121, 25.4651),
-              locationBias: _locationBias);
-
-      setState(() {
-        _predictions = result;
-        _predicting = false;
-      });
-    } catch (err) {
-      setState(() {
-        _predictErr = err;
-        _predicting = false;
-      });
-    }
-  }
-
-  void _onItemClicked(AutocompletePrediction? item) {
-    if (item != null) {
-      _fetchPlaceIdController.text = item.placeId;
-    }
-  }
-
-  Widget _buildPredictionItem(AutocompletePrediction? item) {
-    if (item == null) {
-      return Container();
-    }
-    return InkWell(
-      onTap: () => _onItemClicked(item),
-      child: Column(children: [
-        Text(item.fullText),
-        Text(item.primaryText + ' - ' + item.secondaryText),
-        const Divider(thickness: 2),
-      ]),
-    );
-  }
-
-  Widget _buildErrorWidget(dynamic err) {
-    final ThemeData theme = Theme.of(context);
-    final String errorText = err == null ? '' : err.toString();
-    return Text(errorText,
-        style: theme.textTheme.caption?.copyWith(color: theme.errorColor));
-  }
-
-  List<Widget> _buildPredictionWidgets() {
-    return [
-      // --
-      TextFormField(
-        onChanged: _onPredictTextChanged,
-        decoration: InputDecoration(label: Text("Query")),
-      ),
-      // _countries
-      TextFormField(
-        onChanged: _onCountriesTextChanged,
-        decoration: InputDecoration(label: Text("Countries")),
-        validator: _countriesValidator,
-        autovalidateMode: AutovalidateMode.onUserInteraction,
-        initialValue: _countries.join(","),
-      ),
-      DropdownButton<TypeFilter>(
-        items: TypeFilter.values
-            .map((TypeFilter item) => DropdownMenuItem<TypeFilter>(
-                child: Text(item.name), value: item))
-            .toList(growable: false),
-        value: _placeTypeFilter,
-        onChanged: _onPlaceTypeFilterChanged,
-      ),
-      ElevatedButton(
-        onPressed: _predicting == true ? null : _predict,
-        child: const Text('Predict'),
-      ),
-
-      // -- Error widget + Result
-      _buildErrorWidget(_predictErr),
-      Column(
-        mainAxisSize: MainAxisSize.min,
-        children: (_predictions ?? [])
-            .map(_buildPredictionItem)
-            .toList(growable: false),
-      ),
-      /*Image(
-        image: GoogleMapsPlacesPlatform.ASSET_POWERED_BY_GOOGLE_ON_WHITE,
-      ),*/
-    ];
-  }
+void main() {
+  runApp(const MaterialApp(home: PlacesDemo()));
 }
---
+++ packages/google_maps_places/google_maps_places_ios/example/lib/auto_complete.dart
@@ -0,0 +1,221 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// ignore_for_file: public_member_api_docs
+
+import 'package:collection/collection.dart';
+import 'package:google_maps_places_ios/google_maps_places_ios.dart';
+
+import 'package:google_maps_places_platform_interface/google_maps_places_platform_interface.dart';
+import 'package:flutter/material.dart';
+import 'page.dart';
+
+class AutoCompletePage extends PlacesExampleAppPage {
+  const AutoCompletePage({Key? key})
+      : super(const Icon(Icons.search), 'Places Autocomplete', key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const AutoCompleteBody();
+  }
+}
+
+class AutoCompleteBody extends StatefulWidget {
+  const AutoCompleteBody({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _MyAutoCompleteBodyState();
+}
+
+class _MyAutoCompleteBodyState extends State<AutoCompleteBody> {
+  final GoogleMapsPlacesPlatform _places = GoogleMapsPlacesPlatform.instance;
+
+  String _query = '';
+  List<String> _countries = <String>['fi'];
+  TypeFilter _typeFilter = TypeFilter.address;
+
+  final LatLng _origin = const LatLng(65.0121, 25.4651);
+
+  final LatLngBounds _locationBias = LatLngBounds(
+    southwest: const LatLng(60.4518, 22.2666),
+    northeast: const LatLng(70.0821, 27.8718),
+  );
+
+  bool _findingPlaces = false;
+  dynamic _error;
+
+  List<AutocompletePrediction> _results = <AutocompletePrediction>[];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: _buildBody());
+  }
+
+  Widget _buildBody() {
+    final List<Widget> widgets = _buildQueryWidgets() + _buildResultWidgets();
+
+    return Padding(
+      padding: const EdgeInsets.all(20),
+      child: ListView(children: widgets),
+    );
+  }
+
+  void _onPlaceTypeFilterChanged(TypeFilter? value) {
+    if (value != null) {
+      setState(() {
+        _typeFilter = value;
+      });
+    }
+  }
+
+  String? _countriesValidator(String? input) {
+    if (input == null || input.isEmpty) {
+      return null;
+    }
+
+    return input
+        .split(',')
+        .map((String countryPart) => countryPart.trim())
+        .map((String countryPart) {
+          if (countryPart.length != 2) {
+            return "Use two-letter country codes splitted by ','";
+          }
+          return null;
+        })
+        .where((String? item) => item != null)
+        .firstOrNull;
+  }
+
+  void _onCountriesTextChanged(String countries) {
+    _countries = (countries == '')
+        ? <String>[]
+        : countries
+            .split(',')
+            .map((String item) => item.trim())
+            .toList(growable: false);
+  }
+
+  void _findAction() {
+    if (_findingPlaces || _query.isEmpty) {
+      return;
+    }
+    setState(() {
+      _findingPlaces = true;
+      _results = <AutocompletePrediction>[];
+      _error = null;
+    });
+    _findPlacesAutoComplete();
+  }
+
+  Future<void> _findPlacesAutoComplete() async {
+    try {
+      final List<AutocompletePrediction> result =
+          await _places.findAutocompletePredictions(
+              query: _query,
+              countries: _countries,
+              typeFilter: <TypeFilter>[_typeFilter],
+              origin: _origin,
+              locationBias: _locationBias);
+
+      setState(() {
+        _results = result;
+        _findingPlaces = false;
+      });
+    } catch (error) {
+      setState(() {
+        _error = error;
+        _findingPlaces = false;
+      });
+    }
+  }
+
+  Widget _buildPredictionRow(String title, String value) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: <Widget>[
+        Container(
+            padding: const EdgeInsets.only(right: 8, top: 4, bottom: 4),
+            child: Text(title)),
+        Flexible(child: Text(value, textAlign: TextAlign.end))
+      ],
+    );
+  }
+
+  Widget _buildAutoPredictionItem(AutocompletePrediction? item) {
+    if (item == null) {
+      return Container();
+    }
+    return Padding(
+        padding: const EdgeInsets.only(right: 8),
+        child: Column(children: <Widget>[
+          _buildPredictionRow('FullText:   ', item.fullText),
+          _buildPredictionRow('PrimaryText:   ', item.primaryText),
+          _buildPredictionRow('SecondaryText:   ', item.secondaryText),
+          _buildPredictionRow(
+              'Distance:   ', '${(item.distanceMeters ?? 0) / 1000} km'),
+          _buildPredictionRow('PlaceId:   ', item.placeId),
+          _buildPredictionRow(
+              'PlaceTypes:   ',
+              item.placeTypes
+                  .map((PlaceType placeType) => placeType.name)
+                  .join(', ')),
+          const Divider(thickness: 2),
+        ]));
+  }
+
+  Widget _buildErrorWidget() {
+    final ThemeData theme = Theme.of(context);
+    final String errorText = _error == null ? '' : _error.toString();
+    return Text(errorText,
+        style: theme.textTheme.caption?.copyWith(color: theme.errorColor));
+  }
+
+  List<Widget> _buildQueryWidgets() {
+    return <Widget>[
+      TextFormField(
+        onChanged: (String text) {
+          _query = text;
+        },
+        decoration: const InputDecoration(label: Text('Query')),
+      ),
+      TextFormField(
+        onChanged: _onCountriesTextChanged,
+        decoration: const InputDecoration(label: Text('Countries')),
+        validator: _countriesValidator,
+        autovalidateMode: AutovalidateMode.onUserInteraction,
+        initialValue: _countries.join(','),
+      ),
+      DropdownButton<TypeFilter>(
+        items: TypeFilter.values
+            .map((TypeFilter item) => DropdownMenuItem<TypeFilter>(
+                value: item, child: Text(item.name)))
+            .toList(growable: false),
+        value: _typeFilter,
+        onChanged: _onPlaceTypeFilterChanged,
+      ),
+      ElevatedButton(
+        onPressed: _findAction,
+        child: const Text('Find'),
+      ),
+      Container(padding: const EdgeInsets.only(top: 20))
+    ];
+  }
+
+  List<Widget> _buildResultWidgets() {
+    return <Widget>[
+      if (_error != null)
+        _buildErrorWidget()
+      else if (!_findingPlaces)
+        Column(
+          children:
+              _results.map(_buildAutoPredictionItem).toList(growable: false),
+        )
+      else
+        const Center(child: CircularProgressIndicator()),
+      const Image(
+        image: AssetImage('assets/google_on_white.png'),
+      ),
+    ];
+  }
+}
